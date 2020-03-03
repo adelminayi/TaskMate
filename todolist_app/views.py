0ed456b568eb4699dd6e8cba9c971f3dd2d22ed6
adel <you@example.com>
2020-03-03 23:16:55 +0330

    v12

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -56,6 +56,13 @@ def pending_task(request, task_id):
     return redirect('todolist')
 
 
+def index(request):
+    context = {
+        'index_text': "welcome index page."
+    }
+    return render(request, 'index.html', context)
+
+
 def contact(request):
     context = {
         'contact_text': "welcome contact page."
